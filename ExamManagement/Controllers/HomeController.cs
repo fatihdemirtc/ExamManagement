21cfd05d358d31b9b0d2728713a4ae78fc7e3276
fatihdemirtc <fatihdemirtc@gmail.com>
2018-01-14 13:20:16 +0300

    Soruların getirilmesi
    Soruların getirilmesi wired.com rss

--- ExamManagement/Controllers/HomeController.cs
+++ ExamManagement/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ExamManagement.Helper;
+using ExamManagement.Models;
 
 namespace ExamManagement.Controllers
 {
@@ -10,20 +12,7 @@ namespace ExamManagement.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            ExamXMLModel a=ExamHelper.AllQuestionsGetter();
             return View();
         }
     }
---
+++ ExamManagement/Helper/ExamHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Net;
+using System.Xml.Serialization;
+using ExamManagement.Models;
+
+namespace ExamManagement.Helper
+{
+    public class ExamHelper
+    {
+        public static ExamXMLModel AllQuestionsGetter()
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://www.wired.com/feed/rss");
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+            StreamReader sr = new StreamReader(resp.GetResponseStream());
+
+            XmlSerializer serial = new XmlSerializer(typeof(ExamXMLModel));
+            ExamXMLModel allModel = (ExamXMLModel)serial.Deserialize(sr);
+            sr.Close();
+            return allModel;
+        }
+    }
+}
